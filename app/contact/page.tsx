4d82f5fa740a0d6e539a9e22160b5a39a7286888
Muthumanikandan11 <muthumanikandan11mk@gmail.com>
2026-03-27 18:13:50 +0530

    mobile screen alignment contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,20 +12,20 @@ export default function Home() {
 
       <CloudImages />
 
-      <div className="hero w-[100vw] h-[100vh] flex items-center flex-col bg-gradient-to-b from-[#9FC2E6] to-[#ECDFD1]">
-        <div className="w-[940px] flex flex-col items-center mt-35">
-          <div className="text-[10px] font-bold text-[#614A44] px-3 py-2 bg-[#F4F1EE] rounded-full">CONTACT US</div>
-          <div className="hero-head text-[64px] leading-[88px] font-semibold text-center">Let's connect</div>
-          <div className="hero-content text-[18px] font-normal text-center text-slate-600 w-[400px] mt-4">Have questions about Dreelio? Contact us and we'll be happy to help.</div>
+      <div className="contact-hero w-[100vw] h-[100vh] flex items-center flex-col bg-gradient-to-b from-[#9FC2E6] to-[#ECDFD1]">
+        <div className="contact-hero-body w-[940px] flex flex-col items-center mt-35">
+          <div className="contact-hero-badge text-[10px] font-bold text-[#614A44] px-3 py-2 bg-[#F4F1EE] rounded-full">CONTACT US</div>
+          <div className="contact-hero-head text-[64px] leading-[88px] font-semibold text-center">Let's connect</div>
+          <div className="contact-hero-content text-[18px] font-normal text-center text-slate-600 w-[400px] mt-4 z-10">Have questions about Dreelio? Contact us and we'll be happy to help.</div>
         </div>
 
-        <form action="" className="bg-white/60 flex flex-col gap-6 p-6 rounded-3xl mt-10">
+        <form action="" className="contact-form bg-white/60 flex flex-col gap-6 p-6 rounded-3xl mt-10">
           <div className="row1 flex gap-4">
-            <div className="name-box flex flex-col gap-2">
+            <div className="name-box flex flex-col gap-2 w-[100%]">
               <label htmlFor="name" className="text-[14px] font-semibold text-[#614A44]">Name</label>
               <input type="text" className="bg-white border border-gray-300 rounded-md px-3 py-2" placeholder="Eg. Jane Smith" id="name" />
             </div>
-            <div className="email-box flex flex-col gap-2">
+            <div className="email-box flex flex-col gap-2 w-[100%]">
               <label htmlFor="email" className="text-[14px] font-semibold text-[#614A44]">Email</label>
               <input type="email" className="bg-white border border-gray-300 rounded-md px-3 py-2" placeholder="jane@framer.com" id="email" />
             </div>
